e8d9709006e3c08a5cdebdc4e568595d05701845
jessguo <380633867@qq.com>
2023-05-26 12:03:17 +0800

    Initial Commit

---
+++ IOSApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  IOSApp
+//
+//  Created by 郭点 on 2023/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOSApp/IOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAppApp.swift
+//  IOSApp
+//
+//  Created by 郭点 on 2023/5/26.
+//
+
+import SwiftUI
+
+@main
+struct IOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
